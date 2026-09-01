98edcf160b30dd2e1efadf79d260d9bc33cb42aa
Suity <165407301+suitylab@users.noreply.github.com>
2026-09-01 10:51:42 +0800

    Clean up

--- src/Suity.Wasm/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Suity.Wasm;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-
-await builder.Build().RunAsync();
